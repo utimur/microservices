d5e98a0c0befcb54fd2f0368db182f3e3d659435
utimur <akva_ten@mail.ru>
2020-10-28 19:16:47 +0300

    change auth service

--- authService/authController.js
+++ authService/authController.js
@@ -1,8 +1,17 @@
 const authService = require('./authService')
+const User = require('./user')
 
 class AuthController {
     async registration(req, res) {
-        const {username, password} = req.body
+        try {
+            const {username, password} = req.body
+            const user = await User.create({username, password})
+            const list = await User.findAll()
+            res.json(list)
+        } catch (e) {
+            console.log(e)
+        }
+
     }
 
     async auth(req, res) {
--- authService/index.js
+++ authService/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const authRouter = require('./authRoutes')
 const cors = require('cors')
 const morgan = require('morgan')
+const sequalize = require('./db')
 
 const PORT = process.env.PORT || 8080
 
@@ -12,5 +13,15 @@ app.use(morgan('combined'))
 app.use(express.json())
 app.use('/api', authRouter)
 
+const start = async () => {
+    try {
+        await sequalize.authenticate();
+        await sequalize.sync({ alter: true });
+        app.listen(PORT, () => console.log('server started on port', PORT))
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
 
-app.listen(PORT, () => console.log('server started on port', PORT))
+start()
