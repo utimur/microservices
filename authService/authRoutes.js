d8db71078c184e7208c4fc19126c4949fa5fbf42
utimur <akva_ten@mail.ru>
2020-11-03 18:44:57 +0300

    update routes*

--- authService/authRoutes.js
+++ authService/authRoutes.js
@@ -1,9 +1,13 @@
 const Router = require('express');
 const router = new Router()
 const authController = require('./authController')
+const authMiddleware = require('./middlewares/auth.middleware')
+const ipMiddleware = require('./middlewares/ip.middleware')
 
 router.post('/registration', authController.registration)
-router.post('/login', authController.auth)
-router.get('/auth', authController.getUserByToken)
+router.post('/login', ipMiddleware, authController.auth)
+router.get('/auth', authMiddleware, authController.getUserByToken)
+router.get('/all', authController.getAllUsers)
+router.get('/connections', authMiddleware, authController.getConnections)
 
 module.exports = router
