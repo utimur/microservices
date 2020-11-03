9a8996b0c3026ad829c425abf5ac371b548983d3
utimur <akva_ten@mail.ru>
2020-11-03 18:44:21 +0300

    add login and get connection list functions

--- authService/authController.js
+++ authService/authController.js
@@ -1,25 +1,53 @@
 const authService = require('./authService')
-const User = require('./user')
+const redis = require('./cashe')
 
 class AuthController {
     async registration(req, res) {
         try {
             const {username, password} = req.body
-            const user = await User.create({username, password})
-            const list = await User.findAll()
-            res.json(list)
+            const user = await authService.createUser(username, password)
+            return res.json(user)
         } catch (e) {
-            console.log(e)
+            return res.json({message:e.message})
         }
-
     }
 
     async auth(req, res) {
-
+        try {
+            const {username, password} = req.body
+            const user = await authService.login(username, password)
+            return res.json(user)
+        } catch (e) {
+            return res.json({message:e.message})
+        }
     }
 
     async getUserByToken(req, res) {
+        try {
+            const user = await authService.getUser(req.user.id)
+            return res.json(user)
+        } catch (e) {
+            return res.json({message:e.message})
+        }
+    }
 
+    async getAllUsers(req, res) {
+        try {
+            const users = await authService.getAll()
+            return res.json(users)
+        } catch (e) {
+            return res.json(e.message)
+        }
+    }
+
+    async getConnections(req, res) {
+        try {
+            redis.get(req.user.username, (err, data) => {
+                res.json(JSON.parse(data));
+            })
+        } catch (e) {
+            return res.status(500).json({message: e.message})
+        }
     }
 }
 
--- authService/authService.js
+++ authService/authService.js
@@ -1,11 +1,52 @@
+const {User, Roles} = require('./user');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const {secretKey} = require('./config')
+
 class AuthService {
-    createUser(email, password) {
-
+    async createUser(username, password) {
+        const candidate = await User.findOne({where: {username}})
+        if (candidate) {
+            throw new Error("Пользователь с таким именем уже существует")
+        }
+        const hashPassword = bcrypt.hashSync(password, 5)
+        const user = await User.create({username, password: hashPassword})
+        const role = await Roles.create()
+        await user.addRole(role)
+        return user
     }
 
-    getUser(id) {
-
+    async login(username, password) {
+        const user = await User.findOne({where: {username},include: [{model: Roles, attributes: ['value']}]})
+        if (!user) {
+            throw new Error("Пользователь с таким именем не найден")
+        }
+        const comparePassword = bcrypt.compareSync(password, user.password)
+        if (!comparePassword) {
+            throw new Error("Вы ввели неверный пароль")
+        }
+        const token = jwt.sign(
+            {id: user.id, roles: user.roles, username },
+            secretKey,
+            {expiresIn: '24h'})
+        return {user, token}
     }
+
+    async getUser(id) {
+        const user = await User.findOne({
+            where: {id},
+            include: [{model: Roles, attributes: ['value']}]
+        })
+        if (!user) {
+            throw new Error('Пользователь не найден')
+        }
+        return user;
+    }
+
+    async getAll() {
+        return User.findAll({where:{}, include: [{model: Roles, attributes: ['value']}]})
+    }
+
 }
 
 module.exports = new AuthService()
