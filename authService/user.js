b7fd1d9bf4aa47a368031f5f2b25131f53a48584
utimur <akva_ten@mail.ru>
2020-11-03 18:42:37 +0300

    update user and role scheme

--- authService/user.js
+++ authService/user.js
@@ -1,7 +1,27 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('./db');
+const {roles} = require('./consts')
 
-module.exports = sequelize.define('User', {
+
+const User =  sequelize.define('user', {
+    id: {type: DataTypes.INTEGER, primaryKey:true, autoIncrement: true},
     username: {type: DataTypes.STRING, unique: true},
     password: {type: DataTypes.STRING},
+},)
+
+
+const Roles = sequelize.define('role', {
+    id: {type: DataTypes.INTEGER, primaryKey:true, autoIncrement: true},
+    value: {type: DataTypes.ENUM(roles.USER, roles.ADMIN), defaultValue: roles.USER},
+    userId: {type:DataTypes.INTEGER, references: {model: User, key: 'id'}},
+}, {
+    timestamps:false,
 })
+
+
+User.hasMany(Roles, {
+
+})
+Roles.belongsTo(User, {foreignKey: 'userId'})
+
+module.exports = {User, Roles}
